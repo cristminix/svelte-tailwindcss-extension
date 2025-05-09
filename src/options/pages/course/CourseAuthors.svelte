06eec2261dc31bec542d6364fe5d17f986dacc29
Putra Budiman <cristminix@gmail.com>
2025-05-09 21:59:12 +0700

    ...

---
+++ src/options/pages/course/CourseAuthors.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { TAuthor } from "@/global/db/models/schema"
+  import { authorUrlFromSlug } from "@/global/fn/course/authorUrlFromSlug"
+
+  export let authors: TAuthor[] = []
+  export let noLinks: boolean = false
+</script>
+
+<div class={`mb-2 flex ${!noLinks ? "text-lg" : "text-md"}`}>
+  <span class="italic mr-1">By</span>
+  {#each authors as author, index}
+    <h4>
+      <a href={authorUrlFromSlug(author.slug)} target="_blank">
+        {author.name} <i class="fa fa-external-link" />
+      </a>
+      {#if index < authors.length - 1}
+        <span class="mr-1">,</span>
+      {/if}
+      {#if index === authors.length - 1}
+        <span></span>
+      {/if}
+    </h4>
+  {/each}
+</div>
--- src/options/pages/course/CourseDisplayPage.svelte
+++ src/options/pages/course/CourseDisplayPage.svelte
@@ -21,6 +21,10 @@
   import type MAuthorCourse from "@/global/db/models/MAuthorCourse"
   import Fs from "@/global/classes/Fs"
   import FsImage from "./course-display-page/fn/FsImage.svelte"
+  import { courseUrlFromSlug } from "@/global/fn/course/courseUrlFromSlug"
+  import CourseAuthors from "./CourseAuthors.svelte"
+  import type { T } from "vitest/dist/chunks/environment.LoooBwUu"
+  import { formatDuration } from "@/global/fn/formatDuration"
 
   export let params: any
   export let queryString: any
@@ -38,7 +42,7 @@
   let mThumbnnail: MThumbnail
   let mAuthor: MAuthor
   let mAuthorCourse: MAuthorCourse
-
+  let selectedThumbnail = writable<TThumbnail | null>(null)
   const loadModels = () => {
     mCourse = store.get("Course") as MCourse
     mExerciseFile = store.get("ExerciseFile") as MExerciseFile
@@ -65,12 +69,13 @@
       thumbnails: thumbnails_,
     } = courseDetailsRec
     thumbnails.update((o) => [])
-
+    selectedThumbnail.set(null)
     setTimeout(() => {
       course.update((o) => course_)
       authors.update((o) => authors_)
       exerciseFiles.update((o) => exerciseFiles_)
       thumbnails.update((o) => thumbnails_)
+      selectedThumbnail.set(getLargestThumbnail(thumbnails_))
     }, 512)
   }
   onMount(() => {
@@ -97,31 +102,75 @@
   $: {
     loadData(params.id)
   }
+  const getLargestThumbnail = (sources: TThumbnail[]) => {
+    let largest = 0
+    let selected: TThumbnail | null = null
+    sources.forEach((thumbnail) => {
+      const [width, height] = thumbnail.size.split("x")
+      const size = parseInt(width) * parseInt(height)
+      if (!largest) {
+        largest = size
+      } else {
+        if (size > largest) {
+          largest = size
+          selected = thumbnail
+        }
+      }
+    })
+    return selected
+  }
 </script>
 
 <!-- <code>
   [{JSON.stringify(params)}]
 </code> -->
-<div class="course-detail-page">
-  <JsonView json={$course} />
-  {#if course}
-    <h3 class="text-xl mb-2">{$course?.title}</h3>
-    <p>{$course?.description}</p>
-  {/if}
-  <JsonView json={$authors} />
-  <!-- <JsonView json={$thumbnails} /> -->
-  <h4>Thumbnail</h4>
-  {#each $thumbnails as thumbnail}
-    {#if thumbnail.path}
-      <div class="thumbnail-item">
-        <FsImage
-          {fs}
-          path={thumbnail.path}
-          alt={thumbnail.path}
-          title={thumbnail.size}
-        />
+{#if $course}
+  <div
+    class=" bg-gray-800 rounded-xl overflow-hidden shadow-lg flex flex-col md:flex-row gap-6 p-6"
+  >
+    <!-- Thumbnail -->
+    <div class="flex-shrink-0">
+      <div class="relative">
+        {#if $selectedThumbnail !== null}
+          <FsImage
+            {fs}
+            cls="w-full md:w-[280px] h-[180px] object-cover rounded-lg"
+            path={$selectedThumbnail.path ?? ""}
+            alt={$selectedThumbnail.path ?? ""}
+            title={$selectedThumbnail.size}
+          />
+        {/if}
+
+        <!-- <div
+          class="absolute top-2 left-2 bg-indigo-900 text-white text-xs px-2 py-1 rounded-md font-semibold"
+        >
+          Ae
+        </div> -->
       </div>
-    {/if}
-  {/each}
-  <JsonView json={$exerciseFiles} />
-</div>
+    </div>
+
+    <!-- Content -->
+    <div class="flex-1 space-y-4">
+      <div>
+        <h2 class="text-2xl font-bold leading-snug">
+          {$course?.title}
+        </h2>
+        <p class="text-sm text-gray-400 mt-1">
+          {#each $authors as author, index}
+            {author.name}
+            {#if index < $authors.length - 1},
+            {/if}
+          {/each}
+        </p>
+        <p class="text-sm text-gray-400 flex items-center gap-2 mt-1">
+          <span>⏱️</span>
+          {formatDuration($course?.duration)}
+        </p>
+      </div>
+
+      <p class="text-gray-300 text-sm">
+        {$course?.description}
+      </p>
+    </div>
+  </div>
+{/if}
